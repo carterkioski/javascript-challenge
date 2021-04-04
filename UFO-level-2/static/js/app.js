4309511dfeb0c0e8f4b8458fe9eba1bbbdedd179
carterkioski <carterkioski@gmail.com>
2021-04-03 23:44:10 -0400

    adding level 2 dropw down and filtering

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,36 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//initally loads all data
+createTable(tableData)
+//selects the button
+button = d3.select('#filter-btn')
+button.on('click', handleClick)
+function handleClick(){
+    //once button is clicked it checks calue of input
+    inputFilter = d3.select('#filter-category').property("value")
+    inputValue = d3.select('#filter-value').property("value")
+    if (inputValue && inputFilter){
+        filteredTableData = tableData.filter((item) => item[inputFilter] === inputValue )
+        createTable(filteredTableData)
+    }
+    else {
+        createTable(tableData)
+    }
+}
+
+//Code for adding in rows
+function createTable(ufoData){
+    d3.select('.ufo-data').html("")
+    ufoData.forEach((item) => {
+        table = d3.select('.ufo-data')
+        tr = table.append('tr')
+        tr.append('td').text(item.datetime)
+        tr.append('td').text(item.city)
+        tr.append('td').text(item.state)
+        tr.append('td').text(item.country)
+        tr.append('td').text(item.shape)
+        tr.append('td').text(item.durationMinutes)
+        tr.append('td').text(item.comments)
+    })
+}
