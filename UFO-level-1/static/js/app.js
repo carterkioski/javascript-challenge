93e29f577b7c68248b21d6f916b5afe0ed2510ee
carterkioski <carterkioski@gmail.com>
2021-04-03 22:38:17 -0400

    adding in the table row adding JS code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+tableData.forEach((item) => {
+    table = d3.select('table')
+    tr = table.append('tr')
+    console.log(item.city)
+    tr.append('td').text(item.datetime)
+    tr.append('td').text(item.city)
+    tr.append('td').text(item.state)
+    tr.append('td').text(item.country)
+    tr.append('td').text(item.shape)
+    tr.append('td').text(item.durationMinutes)
+    tr.append('td').text(item.comments)
+}
+)
